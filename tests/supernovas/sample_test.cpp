a2d49e830beea8ff84e6fd9d139c3be45624ffc3
brobeson <brobeson@users.noreply.github.com>
2026-02-21 23:36:14 -0700

    add clang-format

--- tests/supernovas/sample_test.cpp
+++ tests/supernovas/sample_test.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 
-SCENARIO("This test proves out the GH Actions workflow for C++") {
+SCENARIO("This test proves out the GH Actions workflow for C++")
+{
   CHECK(true);
 }
