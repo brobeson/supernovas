8b6576df6827ce4e32eafdab1e6d6bb2ae50d872
brobeson <brobeson@users.noreply.github.com>
2026-03-21 18:57:39 -0600

    Add julian_day functionality
    Add an accessor to the underlying number of days. Add operator==() to
    compare julian_day objects for equality. Add operator""_jd() to allow
    literal julian_day objects. Add test cases for these new functions.
    
    issue #5

--- include/supernovas/julian_clock.hpp
+++ include/supernovas/julian_clock.hpp
@@ -3,22 +3,57 @@
 
 #include <chrono>
 
-namespace novas
-{
-  /// The amount of time into a Julian day
-  using julian_time = std::chrono::nanoseconds;
+namespace novas {
+/// The amount of time into a Julian day
+using julian_time = std::chrono::nanoseconds;
 
-  /// A number of Julian days relative to the epoch 4713 BCE
-  class julian_day
-  {
-  public:
-    using day_type = std::int32_t;
+/// A number of Julian days relative to the epoch 4713 BCE
+class julian_day {
+public:
+  /// The data type of the underlying day count.
+  using day_type = std::int32_t;
 
-    constexpr explicit julian_day(const day_type day = 0) : m_day{day} {}
+  /**
+   * \brief Construct a Julian day from a raw number of days.
+   * \param[in] day The number days since start of the Julian epoch.
+   */
+  constexpr explicit julian_day(const day_type day = 0) : m_day{day} {}
 
-  private:
-    day_type m_day;
-  };
+  /// \return The number of days since the start of the Julian epoch.
+  constexpr day_type day() const noexcept { return m_day; }
+
+private:
+  day_type m_day;
+};
+
+/**
+ * \brief Compare two Julian days for equality
+ * \param[in] a,b Compare these two Julian days.
+ * \retval true \a a.day() == \a b.day()
+ * \retval false \a a.day() != \a b.day()
+ */
+constexpr bool operator==(const julian_day &a, const julian_day &b) {
+  return a.day() == b.day();
+}
+
+namespace literals {
+/**
+ * \brief Construct a literal Julian day.
+ * \param[in] d The number of days since the start of the Julian epoch.
+ * \return The Julian day, as if by `julian_day{d};`
+ * \note To use this literal operator, you must include
+ * `using namespace novas::literals;`.
+ * \warning This operator quietly narrows if
+ * `d > std::numeric_limits<julian_day::day_type>::max()`.
+ * \code{.cpp}
+ * using namespace novas::literals;
+ * constexpr auto jd2000{2'451'544_jd};
+ * \endcode
+ */
+constexpr julian_day operator""_jd(unsigned long long d) noexcept {
+  return julian_day(d);
+}
+} // namespace literals
 } // namespace novas
 
 #endif
--- tests/supernovas/julian_clock_test.cpp
+++ tests/supernovas/julian_clock_test.cpp
@@ -1,8 +1,10 @@
 #include "supernovas/julian_clock.hpp"
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
-SCENARIO("julian_day meets type requirements", "[unit]")
-{
+using namespace novas::literals;
+
+SCENARIO("julian_day meets type requirements", "[unit]") {
   CHECK(std::is_constructible_v<novas::julian_day>);
   CHECK(std::is_default_constructible_v<novas::julian_day>);
   CHECK(std::is_trivially_move_assignable_v<novas::julian_day>);
@@ -12,4 +14,17 @@ SCENARIO("julian_day meets type requirements", "[unit]")
   CHECK(std::is_trivially_copy_constructible_v<novas::julian_day>);
   CHECK(std::is_standard_layout_v<novas::julian_day>);
   CHECK(std::is_integral_v<novas::julian_day::day_type>);
+}
+
+SCENARIO("developers can creat julian_day literals", "[unit]") {
+  CHECK(0_jd == novas::julian_day{});
+  CHECK(2'451'545_jd == novas::julian_day{2'451'545});
+}
+
+SCENARIO("developers can retrieve the raw day number") {
+  // There are no classes of input data for this. Run a few assertions to
+  // ensure it's not hard-coded `return 0` or something like that.
+  CHECK((0_jd).day() == 0);
+  CHECK((1_jd).day() == 1);
+  CHECK(novas::julian_day{-1}.day() == -1);
 }
